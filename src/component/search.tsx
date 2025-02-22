4e77b2fcde57acfd5e392b2a597053fd7f75705a
uktamm <uktamuzoqov2@gmail.com>
2025-02-22 15:20:53 +0500

    tuzatish

--- src/component/search.tsx
+++ src/component/search.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import axios, { AxiosResponse } from 'axios';
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { config } from '../helpers/url';
 import { useQuery } from '@tanstack/react-query';
 import { CountryType } from '../helpers/getCountryType';
